0b4d1db093e5e70168f8e6811bd00df29ecb2a54
MouhamedBourouba <mouhamedmobiledev@gmail.com>
2025-02-04 11:53:50 +0100

    Image cash service

---
+++ regress/lib/data/sources/local_image_cache.dart
@@ -0,0 +1,48 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:crypto/crypto.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:result_dart/result_dart.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Base64LocalImageCache {
+  final SharedPreferences _sp;
+
+  Base64LocalImageCache(this._sp);
+
+  String generateFileName(String key, {String ext = ".png"}) {
+    final bytes = utf8.encode(key);
+    final digest = md5.convert(bytes);
+    return "${digest.toString()}$ext";
+  }
+
+  Future<File> _saveImageLocally(Uint8List imageData, String fileName) async {
+    final directory = await getApplicationDocumentsDirectory();
+    final localFilePath = "${directory.path}/$fileName";
+    final file = File(localFilePath);
+    await file.writeAsBytes(imageData);
+    return file;
+  }
+
+  ResultDart<File, Unit> loadCachedImage(String cacheKey) {
+    if (_sp.containsKey(cacheKey)) {
+      final file = File(_sp.getString(cacheKey)!);
+      if (file.existsSync()) {
+        return file.toSuccess();
+      }
+    }
+    return unit.toFailure();
+  }
+
+  Future<File> cacheImage(
+    String imageBase64,
+    String cacheKey,
+  ) async {
+    final imageData = base64Decode(imageBase64);
+    final file = await _saveImageLocally(imageData, generateFileName(cacheKey));
+    _sp.setString(cacheKey, file.path);
+    return file;
+  }
+}
