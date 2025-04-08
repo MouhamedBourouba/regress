bcd88945ae4b90711385618f4e390f7955c9d23d
MouhamedBourouba <mouhamedmobiledev@gmail.com>
2025-04-08 20:27:36 +0100

    .

--- regress/README.md
+++
@@ -1,3 +0,0 @@
-# regress
-
-A reimagined progres
--- regress/lib/domain/models/exam_notes.dart
+++
@@ -1,7 +0,0 @@
-class ExamNotes {
-  final String subject;
-  final double coefficient;
-  final double grade;
-
-  ExamNotes({required this.subject, required this.coefficient, required this.grade});
-}
